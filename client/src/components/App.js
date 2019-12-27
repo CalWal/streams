6e52350e32a1e2dfdcbe2ff0a7b6a03f12e6ae4b
CalWal <callumtrwallace@gmail.com>
2019-12-27 12:28:51 +1000

    added react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const pageOne = () => {
+  return (
+    <div>
+      Page 1<Link to="/pagetwo">To Page 2</Link>
+    </div>
+  );
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      Page 2<Link to="/">To Page 1</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div> Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={pageOne} />
+          <Route path="/pagetwo" component={pageTwo} />
+        </div>
+      </BrowserRouter>{" "}
+    </div>
+  );
 };
 
 export default App;
