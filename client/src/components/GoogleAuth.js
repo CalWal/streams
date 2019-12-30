2397ccc70621dad362e208ed97c9178427debabb
CalWal <callumtrwallace@gmail.com>
2019-12-30 16:24:40 +1000

    wired up GAPI Library with clientID and Scope to CDM

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "429033860749-duhv5kprhpocng1eg8f688an0p8gulkb.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
